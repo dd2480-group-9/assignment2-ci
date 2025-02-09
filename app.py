8eb238a1f3bf18c3f161fe71f8051e7b69ad56c6
Sebkri555 <sebbe.kristoffersson@gmail.com>
2025-02-09 20:40:13 +0100

    docs(app.py) : Adds documentation for app.py file #11

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, request, jsonify
+"""
+This module provides 2 methods for server handling and HTTP interaction using flask framework
+"""
 
-app = Flask(__name__)
 
+from flask import Flask, request, jsonify   # Importing flask 
+import json                                 # Importing json
+
+app = Flask(__name__)  # Creates one instance of flask
+
+# Defining the initial route
 @app.route('/')
-
 def index():
-    return 'Hello Group 9'
-if __name__ == '__main__':
-    app.run(debug=True, port = 8009, host='0.0.0.0')  # Should be hosted on port 80 + group 9 = 8009
+    
+    return 'Hello Group 9' # Output on HTTP page 
 
+
+# Route that reacts when someone tries to access the /webhook on a browser. 
 @app.route('/webhook', methods=['GET'])
 def webhook():
     if request.method == 'GET':  
         print("GET request received!")  
         return 'You are now in a different route, Congrats !'
     
+# Route that reacts when "input" is given such as webhooks or requests
 @app.route('/webhook', methods=['POST'])
 def post_webhook():
  
@@ -22,5 +30,6 @@ def post_webhook():
         print("POST request received!")  
         return jsonify({'message': 'POST request received on /webhook'}), 200  
 
+# Running the server instance via run command. 
 if __name__ == '__main__':
     app.run(debug=True, port = 8009, host='0.0.0.0') 
