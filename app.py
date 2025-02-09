0523613ba9e163c9389a4dddc3ffb9f1e27cbee5
Sebkri555 <sebbe.kristoffersson@gmail.com>
2025-02-09 20:38:50 +0100

    feat (app.py) : adding simple route #11

--- app.py
+++ app.py
@@ -9,4 +9,18 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True, port = 8009, host='0.0.0.0')  # Should be hosted on port 80 + group 9 = 8009
 
+@app.route('/webhook', methods=['GET'])
+def webhook():
+    if request.method == 'GET':  
+        print("GET request received!")  
+        return 'You are now in a different route, Congrats !'
+    
+@app.route('/webhook', methods=['POST'])
+def post_webhook():
+ 
+    if request.method == 'POST':  
+        print("POST request received!")  
+        return jsonify({'message': 'POST request received on /webhook'}), 200  
 
+if __name__ == '__main__':
+    app.run(debug=True, port = 8009, host='0.0.0.0') 
