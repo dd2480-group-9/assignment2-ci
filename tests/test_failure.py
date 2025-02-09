6fbe62356f483b8845540fa7a4c89eb4de6d5822
Muhammet Eren ozogul <muhammeterenozogul@gmail.com>
2025-02-09 16:36:03 +0100

    docs(test_failure.py): Adds documentation to ./tests/test_failure.py file #9

--- tests/test_failure.py
+++ tests/test_failure.py
@@ -1,4 +1,13 @@
+"""
+tests/test_failure.py
 
+This module includes a basic test that always fails to test ./src/test_runner.py
+is correct.
+"""
 def test_always_fails():
+    """
+    Test that always fails.
 
+    This test is intentionally designed to fail to check pytest is running properly.
+    """
     assert False, "This test is designed to fail intentionally."
