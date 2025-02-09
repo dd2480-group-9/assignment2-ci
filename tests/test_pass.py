3be2707727a6c44a82fda7586c57ba14e8e95901
Muhammet Eren ozogul <muhammeterenozogul@gmail.com>
2025-02-09 16:37:05 +0100

    docs(test_pass.py): Adds documentation to ./tests/test_pass.py file #9

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -1,6 +1,15 @@
+"""
+tests/test_pass.py
 
+This module includes a basic test that always passes to test ./src/test_runner.py
+is correct.
+"""
 def test_always_passes():
+    """
+    Test that always passes.
 
+    This test is intentionally designed to pass to check pytest is running properly.
+    """
     assert True, "This test is designed to pass intentionally."
 
 
