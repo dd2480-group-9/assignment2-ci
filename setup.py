b48f902332d254accb90e7c9342d9163fe7e39f2
youngbin pyo <youngbinpyo@n189-p137.eduroam.kth.se>
2025-02-11 14:54:14 +0100

    docs(setup.py): added GitPython to installing packages #12

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
         "flask>=2.0.0",             # Match dependencies in requirements.txt
         "pytest>=8.0.0",
         "pdoc>=14.0.0",
-        "pip>=23.0"
+        "pip>=23.0",
+        "GitPython>=0.3.2"
     ],
     python_requires=">=3.13",
 )
